34438cf24438c43b81b8315ba18c1a1283e5f53b
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-03-22 11:12:19 +0200

    s

--- new.js
+++ new.js
@@ -915,9 +915,32 @@ const players = [
 // };
 
 // console.log(calculateTotalBalance(users));
-const getTotalFriendCount = users => {
-  return users.reduce((acc, curent) => {
-    return acc + curent.friends.length;
-  },0);
-}
-console.log(getTotalFriendCount(users));
+// const getTotalFriendCount = users => {
+//   return users.reduce((acc, curent) => {
+//     return acc + curent.friends.length;
+//   },0);
+// }
+// console.log(getTotalFriendCount(users));
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//   { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
+// Пиши код ниже этой строки
+
+// const sortedByAuthorName = [...books].sort((a,b)=> a.author.localeCompare(b.author));
+
+// const sortedByReversedAuthorName = [...books].sort((a,b)=> b.author.localeCompare(a.author));
+
+// const sortedByAscendingRating = [...books].sort((a,b)=> a.rating-b.rating);
+
+// const sortedByDescentingRating = [...books].sort((a,b)=> b.rating-a.rating);
+
+const sortByDescendingFriendCount = users => {
+  return [...users].sort((a, b) => b.friends.localCompare(a));
+};
+
+
+console.table(sortByDescendingFriendCount(users));
