fd0e23e6434ca9b067e35a59c98f1b1dae96357a
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-05-03 22:24:04 +0300

    add

--- first.js
+++ first.js
@@ -32,54 +32,69 @@
 //     об успешной авторизацие
 // */
 
-const login = document.querySelector("#login");
-const password = document.querySelector("#password");
-const checkBox = document.querySelector("#checkBox");
-const submitButton = document.querySelector("#submitButton");
-const msgBox = document.querySelector("#msgBox");
+// const login = document.querySelector("#login");
+// const password = document.querySelector("#password");
+// const checkBox = document.querySelector("#checkBox");
+// const submitButton = document.querySelector("#submitButton");
+// const msgBox = document.querySelector("#msgBox");
 
-const loginInfo = document.querySelector("#loginInfo");
-const passwordInfo = document.querySelector("#passwordInfo");
+// const loginInfo = document.querySelector("#loginInfo");
+// const passwordInfo = document.querySelector("#passwordInfo");
 
-login.addEventListener("focus", e => {
-  loginInfo.style.display = "block";
-});
+// login.addEventListener("focus", e => {
+//   loginInfo.style.display = "block";
+// });
 
-login.addEventListener("blur", e => {
-  loginInfo.style.display = "none";
-  if (login.value.length < 4) {
-    login.classList.add("error");
-  } else {
-    // Вариант 1
-    if (login.classList.contains("error")) {
-      login.classList.remove("error");
-    }
-  }
-});
+// login.addEventListener("blur", e => {
+//   loginInfo.style.display = "none";
+//   if (login.value.length < 4) {
+//     login.classList.add("error");
+//   } else {
+//     // Вариант 1
+//     if (login.classList.contains("error")) {
+//       login.classList.remove("error");
+//     }
+//   }
+// });
 
-password.addEventListener("focus", e => {
-  passwordInfo.style.display = "block";
-});
+// password.addEventListener("focus", e => {
+//   passwordInfo.style.display = "block";
+// });
 
-password.addEventListener("blur", e => {
-  passwordInfo.style.display = "none";
-  if (password.value.length < 8 || password.value.length > 30) {
-    password.classList.add("error");
-  } else {
-    // Вариант 2
-    password.classList.remove("error");
-  }
-});
+// password.addEventListener("blur", e => {
+//   passwordInfo.style.display = "none";
+//   if (password.value.length < 8 || password.value.length > 30) {
+//     password.classList.add("error");
+//   } else {
+//     // Вариант 2
+//     password.classList.remove("error");
+//   }
+// });
 
-submitButton.addEventListener("click", e => {
-  e.preventDefault();
-  if (
-    login.classList.contains("error") ||
-    password.classList.contains("error") ||
-    !checkBox.checked
-  ) {
-    msgBox.innerHTML = "Условия не соответствуют требованиям!";
-  } else {
-    msgBox.innerHTML = "Авторизация прошла успешно!";
-  }
-});
+// submitButton.addEventListener("click", e => {
+//   e.preventDefault();
+//   if (
+//     login.classList.contains("error") ||
+//     password.classList.contains("error") ||
+//     !checkBox.checked
+//   ) {
+//     msgBox.innerHTML = "Условия не соответствуют требованиям!";
+//   } else {
+//     msgBox.innerHTML = "Авторизация прошла успешно!";
+//   }
+// });
+function findOdd(A) {
+  let num = null;
+  const result = A.reduce(function(acc, el) {
+    acc[el] = (acc[el] || 0) + 1;
+    return acc;
+  }, {});
+ for (const key in result) {
+   if (result[key] % 2 !== 0) {
+    return num = key;
+  } 
+ }
+  return num;
+}
+
+  console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
